d79df10d8872e2186a102d669ea9eca89b4eb921
assaadaladas <assaadaladas@hotmail.com>
2025-06-02 23:19:08 +0300

    add db context to registered services

--- GarageClientAPI/Program.cs
+++ GarageClientAPI/Program.cs
@@ -1,6 +1,11 @@
+using GarageClientAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<GarageClientContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GarageClientConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
